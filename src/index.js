0530900d20a1319da8a3e27cee3019db95d06a4d
Olivier Bitsch <olivierb@olivier-840.iabsis.local>
2024-08-26 19:42:58 +0200

    Fix various stuffs

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
 import "bootstrap/dist/js/bootstrap.bundle.min"; // Import Bootstrap JS (includes Popper.js)
 import "@coreui/coreui"; // Import CoreUI JS
 import "@coreui/coreui/dist/js/coreui.bundle.min.js"; // Import Popper.js
-import "simplebar/dist/simplebar.min.js";
-// import "chart.js/dist/chart.umd.js";
 import "@coreui/chartjs/dist/js/coreui-chartjs.js";
+import "@coreui/utils/dist/umd/index.js";
+import "simplebar/dist/simplebar.min.js";
+import "simplebar/dist/simplebar.min.css";
+
+/*import "chart.js/dist/chart.umd.js";*/
+import "chart.js/auto";
+
 import "./styles.css"; // Import your custom CSS
+import "./scss/style.scss";
 
 // Your custom JavaScript code
 $(document).ready(function () {
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+          { from: 'node_modules/@coreui/icons/sprites' }
+      ]
+  }),
+  ],
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "static/js"),
+    path: path.resolve(__dirname, "static"),
   },
   module: {
     rules: [
@@ -20,6 +28,17 @@ module.exports = {
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
       },
     ],
   },
