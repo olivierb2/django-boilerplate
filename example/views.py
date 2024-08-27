e34d67bc9a60dc7b6cacc66bae850cebfd01738f
Olivier Bitsch <olivier.b@iabsis.com>
2024-08-27 14:10:10 +0200

    Add url and example

--- example/views.py
+++ example/views.py
@@ -3,5 +3,37 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 
-class Example(TemplateView):
+class ExampleIndex(TemplateView):
     template_name = "index.html"
+
+
+class ExampleButtons(TemplateView):
+    template_name = "buttons.html"
+
+
+class ExampleCards(TemplateView):
+    template_name = "cards.html"
+
+
+class ExampleUtilitiesColor(TemplateView):
+    template_name = "utilities-color.html"
+
+
+class ExampleUtilitiesBorder(TemplateView):
+    template_name = "utilities-border.html"
+
+
+class ExampleUtilitiesAnimation(TemplateView):
+    template_name = "utilities-animation.html"
+
+
+class ExampleUtilitiesOther(TemplateView):
+    template_name = "utilities-other.html"
+
+
+class ExampleCharts(TemplateView):
+    template_name = "charts.html"
+
+
+class ExampleTables(TemplateView):
+    template_name = "tables.html"
