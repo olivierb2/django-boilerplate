a38cfd4df8a847fb1d13b2e3e3d32292b4c24642
Olivier Bitsch <olivier.b@iabsis.com>
2024-09-23 10:24:51 +0200

    Initial release

--- sbadmin2/src/index.js
+++ sbadmin2/src/index.js
@@ -16,6 +16,19 @@ import "startbootstrap-sb-admin-2/js/sb-admin-2.js";
 import "chart.js";
 import "datatables.net-bs4";
 
+import "select2";
+import "select2/dist/css/select2.min.css";
+import "@ttskch/select2-bootstrap4-theme/dist/select2-bootstrap4.min.css";
+
+$(document).ready(function () {
+  $(".select2").select2({
+    theme: "bootstrap4",
+    width: '100%',
+  });
+});
+
+import "./css/styles.css";
+
 // Your custom JavaScript code
 $(document).ready(function () {
   console.log("Libs are ready!");
